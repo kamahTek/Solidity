fbd7d4bd12f4059e1d2a65201c9099d3c20f827a
kamahTek <jimhkamah@gmail.com>
2025-04-11 11:55:00 +0300

    created my first solidity contract

--- Contracts/Sessions/Lesson_1.sol
+++ Contracts/Sessions/Lesson_1.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+//created my first solidity contract
 contract greetings{
     string public greeting="hello Solidity";
     string public welcome="welcome to solidity world";
