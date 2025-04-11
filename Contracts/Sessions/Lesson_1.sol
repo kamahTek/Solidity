2c0bafefc7c23e43f5de0a6b7f210b39d80aacf4
kamahTek <jimhkamah@gmail.com>
2025-04-11 11:27:05 +0300

    familiarizing with solidity

---
+++ Contracts/Sessions/Lesson_1.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract greetings{
+    string public greeting="hello Solidity";
+    string public welcome="welcome to solidity world";
+
+}
