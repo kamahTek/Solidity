42414f7f50bdf2ce6912b5b5a6dfc88a7d2aafd6
kamahTek <jimhkamah@gmail.com>
2025-05-06 22:25:04 +0300

    created a modifier to check if msg.sender is the owner

---
+++ Solidity/Solidity/Contracts/Projects/twitter_modifiers.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract twitterModifiers{
+
+    uint16 public MAX_TWEET_LENGTH = 5;
+
+    //create a twitter struct
+    struct Tweet {
+        uint256 id;
+        address author;
+        string content;
+        uint timestamp;  
+        uint likes ;  
+    }
+    
+    //create a mapping with an array of strings(tweets) of each owner
+    mapping (address => Tweet[]) public tweets;
+
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "YOU ARE NOT THE OWNER");
+        _;
+    }
+
+    function createTweet(string memory tweetContent)public {
+
+        //Require that all of our tweets is less or equal to MAX_TWEET_LENGTH
+        require((bytes(tweetContent).length <= MAX_TWEET_LENGTH), "TWEET TOO LOOOONG BROOOOOOO!!!!");
+
+        //Feeding our structs with data
+        Tweet memory newTweets = Tweet({
+            id: tweets[msg.sender].length,
+            author: msg.sender,
+            content: tweetContent,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweets);
+    }
+
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner{
+        MAX_TWEET_LENGTH = newTweetLength;
+    }
+
+    // Get all of a users Tweets
+    function getTweets(address _author) external view returns (Tweet[] memory){
+        return tweets[_author];
+    }
+
+    //Get the tweet by its index
+    function singleTweet(address _author, uint _i) external view returns (Tweet memory){
+        return tweets[_author][_i];
+    } 
+
+}
